c1bbd0aac73e23c8eb2e0030d0fcf8d3f0163a8b
ishanshastri <94653377+ishanshastri@users.noreply.github.com>
2022-01-05 00:15:29 -0500

    Add files via upload

---
+++ Eliza_Test/TestFramework/PatternTest.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Eliza;
+
+namespace ElizaTest
+{
+    /// <summary>
+    /// Tests a pattern.
+    /// </summary>
+    abstract class PatternTest
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PatternTest"/> class.
+        /// </summary>
+        /// <param name="pattern">The pattern being tested.</param>
+        protected PatternTest
+            (Pattern pattern)
+        {
+            this._pattern = pattern;
+            this.AddTests();
+        }
+
+        #endregion
+
+        #region Public properties
+
+        /// <summary>
+        /// Gets or sets the number of patterns tested.
+        /// </summary>
+        public static int PatternsTestedCount {
+            get;
+            internal set;
+        }
+
+        /// <summary>
+        /// Gets or sets the number of rules tested.
+        /// </summary>
+        public static int RulesTestedCount {
+            get;
+            internal set;
+        }
+
+        /// <summary>
+        /// Gets or sets the number of rules tested.
+        /// </summary>
+        public static int ResponsesTestedCount {
+            get;
+            internal set;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Resets the test counters.
+        /// </summary>
+        public static void ResetCounters()
+        {
+            PatternTest.PatternsTestedCount = 0;
+            PatternTest.RulesTestedCount = 0;
+            PatternTest.ResponsesTestedCount = 0;
+        }
+
+        /// <summary>
+        /// Runs the tests in this instance.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        public void Run
+            (ILogger logger)
+        {
+            PatternTest.PatternsTestedCount++;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(string.Format("Testing pattern {0} ", this._pattern.GetType().FullName));
+            Console.ResetColor();
+            IEliza eliza = new Eliza.Eliza(logger);
+            foreach (Test test in this._ruleTests) {
+                PatternTest.RulesTestedCount++;
+                test.Run(this._pattern, eliza.Synonyms, logger);
+            }
+        }
+
+        #endregion
+
+        #region Protected methods
+
+        /// <summary>
+        /// Adds a test to the set of rule tests to be run.
+        /// </summary>
+        protected void AddRuleTest
+            (Test test)
+        {
+            this._ruleTests.Add(test);
+        }
+
+        #endregion
+
+        #region Abstract methods
+
+        /// <summary>
+        /// Adds the pattern's tests.
+        /// </summary>
+        abstract protected void AddTests();
+
+        #endregion
+
+        #region Fields
+
+        /// <summary>
+        /// The pattern being tested.
+        /// </summary>
+        private Pattern _pattern;
+
+        /// <summary>
+        /// The tests for each of this pattern's rules.
+        /// </summary>
+        private List<Test> _ruleTests = new List<Test>();
+
+        #endregion
+    }
+}
---
+++ Eliza_Test/TestFramework/Test.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Eliza;
+
+namespace ElizaTest
+{
+    /// <summary>
+    /// A collection of user inputs and expected responses that need to be tested.
+    /// </summary>
+    class Test
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Test"/> class.
+        /// </summary>
+        /// <param name="name">The name of the test.</param>
+        public Test
+            (string name)
+        {
+            this._name = name;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Adds an input and response to be checked.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="response">The response.</param>
+        public void AddCheck
+            (string input,
+             string response)
+        {
+            this._inputsAndResponses.Add(new Tuple<string, string>(input, response));
+        }
+
+        /// <summary>
+        /// Runs the test on a pattern.
+        /// </summary>
+        /// <param name="pattern">The pattern.</param>
+        /// <param name="synonyms">The synonyms.</param>
+        /// <param name="logger">The logger.</param>
+        public void Run
+            (Pattern pattern,
+             Dictionary<string, List<string>> synonyms,
+             ILogger logger)
+        {
+            Console.Write(string.Format("  {0} {1}...", pattern.GetType().FullName, this._name));
+            DateTime start = DateTime.Now;
+            IEliza eliza = new Eliza.Eliza(logger);
+            bool testPassed = true;
+            string error = null;
+            foreach(Tuple<string, string> inputAndResponse in this._inputsAndResponses) {
+
+                // Convert the input to the form expected by the pattern
+                string input = inputAndResponse.Item1;
+                input = input.ToUpper();
+                input = input.Replace(".", string.Empty);
+                input = input.Replace(",", string.Empty);
+                input = input.Replace("!", string.Empty);
+                input = input.Replace("?", string.Empty);
+
+                // Get the response
+                string response = pattern.GenerateResponse(input, synonyms, eliza.Memory, logger);
+                PatternTest.ResponsesTestedCount++;
+                error = string.Format("Expected \"{0}\" but received \"{1}\"",
+                                       inputAndResponse.Item2, response);
+
+                // Test the response - stop the test on failure
+                testPassed = response == inputAndResponse.Item2;
+                if (!testPassed) {
+                    break;
+                }
+            }
+
+            // Display test result
+            TimeSpan elapsedTime = DateTime.Now - start;
+            Console.ForegroundColor = testPassed ? ConsoleColor.Green : ConsoleColor.Red;
+            Console.WriteLine(" {0} in {1} mSec", testPassed ? "PASSED" : "FAILED", elapsedTime.TotalMilliseconds);
+            Console.ResetColor();
+
+            // Invoke debugger if the test failed
+            if (!testPassed) {
+                Debug.Assert(false, error);
+            }
+
+        }
+
+        #endregion
+
+        #region Fields
+
+        /// <summary>
+        /// The name of the test.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// The list of inputs and expected responses.
+        /// </summary>
+        private List<Tuple<string, string>> _inputsAndResponses = new List<Tuple<string, string>>();
+
+        #endregion
+    }
+}
